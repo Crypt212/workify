8a60a689960132a564e57311bac4a49f2327a6f9
Ahmed <ahmed.elesseily.d@gmail.com>
2025-08-08 19:57:12 +0300

    Fixed auth and routing to the right user type pages

---
+++ app/Http/Middleware/EnsureIsEmployer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureIsEmployer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user()->identity !== 'employer') {
+            abort(403, 'Employer access only');
+        }
+        return $next($request);
+    }
+}
